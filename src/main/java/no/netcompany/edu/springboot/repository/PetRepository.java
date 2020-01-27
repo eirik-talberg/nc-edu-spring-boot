f6288691c6171dbdb71ab44c46032fbe1703a4be
Eirik W. Talberg <eirikt@netcompany.com>
2020-01-27 10:21:39 +0100

    Added repository annotation to PetRepository

--- src/main/java/no/netcompany/edu/springboot/repository/PetRepository.java
+++ src/main/java/no/netcompany/edu/springboot/repository/PetRepository.java
@@ -2,5 +2,7 @@ package no.netcompany.edu.springboot.repository;
 
 import no.netcompany.edu.springboot.model.Pet;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetRepository extends CrudRepository<Pet, Long> {}
